0c235e1a8184a4dc17ad291f7a0ac1a56b1e32c2
SleepLessKnight <mailtocode@gmail.com>
2024-03-20 20:19:20 -0700

    changed 1 more comment

--- loop_demo_1.py
+++ loop_demo_1.py
@@ -1,4 +1,4 @@
-# Write a program to print odd numbers starting from the number 7 and ending at the number 91.
+# Write a program to print odd numbers from the number 7 and ending at the number 91.
 # The number 7 and 91 should be printed.
 
 for odd in range(7, 92, 2):
